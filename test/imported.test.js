377c26e8c7435841cf43e25abf0f1bedea66d35e
Amol Kulkarni <amol.kulkarni@synerzip.com>
2015-06-09 18:56:28 +0530

    Minor fixes related to updateOrCreate and findOrCreate.

--- test/imported.test.js
+++ test/imported.test.js
@@ -6,8 +6,8 @@ describe('firebase imported features', function () {
   });
 
   require('loopback-datasource-juggler/test/datatype.test.js');
-  require('loopback-datasource-juggler/test/common.batch.js');
-  require('loopback-datasource-juggler/test/default-scope.test.js');
-  require('loopback-datasource-juggler/test/include.test.js');
+  require('loopback-datasource-juggler/test/basic-querying.test.js');
+  require('loopback-datasource-juggler/test/manipulation.test.js');
+  require('loopback-datasource-juggler/test/hooks.test.js');
 
 });
